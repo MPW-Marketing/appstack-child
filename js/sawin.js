78fe37f70b0907021df9f318b169bec19c339a3e
Damon Maldonado <damon@mpwmarketing.com>
2016-02-26 10:15:10 -0500

    updates

--- js/sawin.js
+++ js/sawin.js
@@ -33,3 +33,43 @@ jQuery(document).ready(function(){
 	
 });
 }*/
+
+
+jQuery(document).ready(function() {
+	jQuery('a[href*=#]').unbind('click');
+   jQuery('a[href*=#]').bind('click', function(e) {
+       
+	var target = $=jQuery(this).attr("href");
+	if ( jQuery(window).innerWidth() < 718 ) { 
+		var offset  = jQuery('.header-banner-top .main-navigation').height() - 50;
+	}
+	else {
+	var offset  = jQuery('.header-banner-top .main-navigation').height() - 20;
+    }
+	jQuery('html, body').stop().animate({ scrollTop: jQuery(target).offset().top - offset}, 500, function() {
+	     //location.hash = target;  //attach the hash (#jumptarget) to the pageurl
+	});
+			
+	return false;
+   });
+  
+});
+
+if ( jQuery(window).innerWidth() < 718 ) {
+	jQuery('.menu-item-has-children > a').unbind('click');
+	    jQuery('.menu-item-has-children').each(function(){
+	    	jQuery(this).addClass("closed");
+	    })
+	jQuery('.menu-item-has-children > a').on('click', function(e) {
+			jQuery(this).toggleClass("expanded-item");
+			jQuery(this).parent('.menu-item-has-children').toggleClass("closed");
+	      	jQuery(this).parent().toggleClass("expanded");	
+	      	jQuery(this).next('.sub-menu').toggleClass("pressed");
+	      	console.log(jQuery(this).parent().hasClass(''));
+	      	if (jQuery(this).parent().hasClass('expanded')) {
+	      		console.log('closed');
+	      		e.preventDefault();	      	
+	      }
+	    });
+
+}
